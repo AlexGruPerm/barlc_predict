9ef94ee8bec30d5b25fb4d3287cc5455c4532e93
AlexGruPerm <ugr@bk.ru>
2019-05-21 17:46:47 +0500

    Successful run local, but errors in Spark (errs related with Spark cluster).

--- src/main/scala/pred/Main.scala
+++ src/main/scala/pred/Main.scala
@@ -1,5 +1,6 @@
 package pred
 
+import org.slf4j.{Logger, LoggerFactory}
 import akka.actor._
 import akka.http.scaladsl._
 import akka.http.scaladsl.model.HttpCharsets._
@@ -8,56 +9,54 @@ import akka.http.scaladsl.model._
 import akka.stream._
 import akka.stream.scaladsl._
 import akka.util.Timeout
-import org.slf4j.{Logger, LoggerFactory}
 
 import scala.concurrent._
 import scala.concurrent.duration._
 
+
 object Main extends App {
   val logger: Logger = LoggerFactory.getLogger(getClass.getName)
   logger.info("BEGIN - Constructor of class Main from pred package.")
-/*
-  implicit val system = ActorSystem()
-  implicit val materializer = ActorMaterializer()
-*/
+
+  val mlpcModel1 = new MlpcModel1
+
   implicit val system = ActorSystem("barclpred-service")
   implicit val materializer = ActorMaterializer()
 
   implicit val executionContext = system.dispatcher
   implicit val timeout = Timeout(10 seconds)
 
-  val shortTimeout = 10.seconds
+  //val shortTimeout = 10.seconds
   val listenPort = 8081
-
-  val serverSource = Http().bind(interface = "0.0.0.0", port = listenPort)
-
-  val accur :Double = 0.8432
+  val serverSource = Http().bind(interface = "192.168.122.219", port = listenPort) //0.0.0.0
 
   val reqHandler: HttpRequest => Future[HttpResponse] = {
 
     case req@HttpRequest(HttpMethods.POST, Uri.Path("/recalc"), _, ent, _)
-    =>
-      logger.info("request (0) "+req.uri+" - "+req.method)
+    => logger.info("request (0) "+req.uri+" - "+req.method)
       Future.successful {
-        HttpResponse(StatusCodes.OK,
-          entity = "Jello 0 POST")
+        HttpResponse(StatusCodes.OK, entity = "Hello 0 POST")
       }
 
     case req@HttpRequest(HttpMethods.GET, Uri.Path("/accur"), _, ent, _)
-    =>
-      logger.info("request (1) "+req.uri+" - "+req.method)
-      val mlpcModel1 = new MlpcModel1
+    => logger.info("request (1) "+req.uri+" - "+req.method)
+
+      try {
+        val resAccur = mlpcModel1.getPredictionByModel
+        logger.info("resAccur="+resAccur)
+      } catch {
+        case ex: Throwable => logger.info(ex.getLocalizedMessage)
+      }
+
       Future.successful {
-        HttpResponse(StatusCodes.OK,
-          entity = "Test set accuracy = ["+mlpcModel1.accur+"]")
+        HttpResponse(StatusCodes.OK, entity = "Test set accuracy = ["+mlpcModel1.getPredictionByModel+"]")
       }
 
     case httpReq : HttpRequest =>
       logger.info("request (2) "+httpReq.uri+" - "+httpReq.method)
       Future.successful {
         HttpResponse(
-          StatusCodes.NotFound,
-          entity = HttpEntity(`text/plain` withCharset `UTF-8`, "<html><body>Not found!</body></html>")
+          StatusCodes.NotFound, entity = HttpEntity(`text/plain` withCharset `UTF-8`, "<html><body>Not found!</body></html>")
         )
       }
   }
--- src/main/scala/pred/MlpcModel1.scala
+++ src/main/scala/pred/MlpcModel1.scala
@@ -46,6 +46,12 @@ class MlpcModel1 {
     .config("spark.jars", "/root/barclpred.jar")
     .getOrCreate()
 
+  import com.datastax.spark.connector.cql.CassandraConnectorConf
+  import org.apache.spark.sql.cassandra._
+  spark.setCassandraConf("Test Cluster", CassandraConnectorConf.ConnectionHostParam.option("192.168.122.192"))
+
+  logger.info("SparkSession.builder() - Successful")
+
   def showLogs(ds :Dataset[Form]) = logger.info(" SIZE = "+ds.count)
 
   def getFormsDb(TickerID :Int, BarWidthSec: Int) :Dataset[Form] = {
@@ -67,30 +73,61 @@ class MlpcModel1 {
         col("formprops")("acf_2_bws").as("acf_2_bws").cast("Double")
       ).as[Form]}
 
-  val ds :Dataset[Form] = Seq(1,3,5).map(elm => getFormsDb(elm,30)).reduce(_ union _)
-  ds.cache()
-  showLogs(ds)
+  def readTrainData = {
+    val ds :Dataset[Form] = Seq(1,3,5).map(elm => getFormsDb(elm,30)).reduce(_ union _)
+    ds.cache()
+    showLogs(ds)
+    ds
+  }
 
-  val stages = new ListBuffer[PipelineStage]()
-  stages += new StringIndexer().setInputCol("frmconfpeak").setOutputCol("confpeakIndex")
-  stages += new StringIndexer().setInputCol("tcvolprofile").setOutputCol("tcvolprofileIndex")
-  stages += new StringIndexer().setInputCol("res_type").setOutputCol("label")
-  stages += new VectorAssembler().setInputCols(Array("tcvolprofileIndex","sps","acf_1_bws","acf_2_bws","confpeakIndex")).setOutputCol("features")
-  stages += new SQLTransformer().setStatement("SELECT label, features FROM __THIS__")
-
-  val MLPCclassif = new MultilayerPerceptronClassifier().setLayers(Array[Int](5, 9, 5, 2)).setLabelCol("label").setFeaturesCol("features").setBlockSize(128).setSeed(1234L).setMaxIter(10)
-  stages += MLPCclassif
+  val ds = readTrainData
 
   val splits = ds.randomSplit(Array(0.7, 0.3), seed = 1234L)
   val train = splits(0)
   val test = splits(1)
 
-  val estimator = new Pipeline().setStages(stages.toArray)
-  val model = estimator.fit(train)
-  val mlpc_predictions = model.transform(test)
-  val predictionAndLabels = mlpc_predictions.select("prediction", "label")
-  val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
-  val accur = evaluator.evaluate(predictionAndLabels)
-  //val model = estimator.fit(ds)
+  def calcModel = {
+    logger.info("begin build stages.")
+    val stages = new ListBuffer[PipelineStage]()
+    stages += new StringIndexer().setInputCol("frmconfpeak").setOutputCol("confpeakIndex")
+    stages += new StringIndexer().setInputCol("tcvolprofile").setOutputCol("tcvolprofileIndex")
+    stages += new StringIndexer().setInputCol("res_type").setOutputCol("label")
+    stages += new VectorAssembler().setInputCols(Array("tcvolprofileIndex","sps","acf_1_bws","acf_2_bws","confpeakIndex")).setOutputCol("features")
+    stages += new SQLTransformer().setStatement("SELECT label, features FROM __THIS__")
+
+    val MLPCclassif = new MultilayerPerceptronClassifier().setLayers(Array[Int](5, 9, 5, 2)).setLabelCol("label").setFeaturesCol("features").setBlockSize(128).setSeed(1234L).setMaxIter(10)
+    stages += MLPCclassif
+    val estimator = new Pipeline().setStages(stages.toArray)
+    val model = estimator.fit(train)
+    val mlpc_predictions = model.transform(test)
+    val predictionAndLabels = mlpc_predictions.select("prediction", "label")
+    val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+    val accur = evaluator.evaluate(predictionAndLabels)
+    logger.info("accur by test data = "+accur)
+    estimator.fit(ds)
+  }
+
+  val predModel = calcModel
+
+  def getPredictionByModel:String = {
+    logger.info("Call funciton getPredictionByModel")
+    val inputData = ds.sample(false, 0.05).limit(1) //INPUT DS ONE ROW
+    logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    logger.info("INSIDE getPredictionByModel")
+
+   //    val tdFrmConfPeak  = inputData.select("frmconfpeak").first().getString(0)
+
+    val tdForm  = inputData.first()
+
+    logger.info("Input data is count="+inputData.count()+" tdForm="+tdForm)
+    logger.info("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    val mlpc_predictions_res = predModel.transform(inputData)
+    val predictionAndLabels = mlpc_predictions_res.select("prediction", "label")
+    val evaluator = new MulticlassClassificationEvaluator().setMetricName("accuracy")
+    val accur = evaluator.evaluate(predictionAndLabels)
+    val predValue = predictionAndLabels.first().getDouble(0)
+    val labelVal = predictionAndLabels.first().getDouble(1)
+    "tdForm = "+tdForm+" predValue = "+predValue+" labelVal = "+labelVal+" accur = "+accur
+  }
 
 }
